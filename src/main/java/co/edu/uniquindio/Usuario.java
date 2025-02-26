5beffaa30769c517dfc367459fea9c0e79dbcd1d
GuevaraSnow <Santiguevar420@gmail.com>
2025-02-25 23:16:47 -0500

    Atributos Usuario
    Se agregan los atributos básicos de usuario

--- src/main/java/co/edu/uniquindio/Usuario.java
+++ src/main/java/co/edu/uniquindio/Usuario.java
@@ -1,5 +1,13 @@
 package co.edu.uniquindio;
 
 public class Usuario {
+        private String id;
+        private String nombre;
+        private String direccion;
+        private String email;
+        private String password;
+        private boolean estado;
 
-}
+    }
+
+
